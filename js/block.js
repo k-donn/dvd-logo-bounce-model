594b2ac15636c1ed8d6e0c4eba28f67aaa6bc741
k-donn <mitchell@kevinandjudy.com>
2020-01-21 15:51:08 -0500

    Fixed performance drain

--- js/block.js
+++ js/block.js
@@ -154,7 +154,7 @@ class Block {
 		this.canvasCtx.fillStyle = "#D3D3D3";
 		this.canvasCtx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
 
-		this.drawGrid();
+		// this.drawGrid();
 
 		// The rect block
 		this.canvasCtx.fillStyle = "#32CD32";
@@ -176,15 +176,51 @@ class Block {
 	}
 
 	drawGrid() {
-		for (let x = 0; x <= this.canvasWidth; x += this.blockWidth) {
-			this.canvasCtx.moveTo(x, 0);
-			this.canvasCtx.lineTo(x, this.canvasHeight);
-			for (let y = 0; y <= this.canvasHeight; y += this.blockHeight) {
-				this.canvasCtx.moveTo(0, y);
-				this.canvasCtx.lineTo(this.canvasWidth, y);
-			}
-		}
-		this.canvasCtx.stroke();
+		let data = `<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+			<defs>
+				<pattern
+					id="smallGrid"
+					width="8"
+					height="8"
+					patternUnits="userSpaceOnUse"
+				>
+					<path
+						d="M 8 0 L 0 0 0 8"
+						fill="none"
+						stroke="gray"
+						stroke-width="0.5"
+					/>
+				</pattern>
+				<pattern
+					id="grid"
+					width="80"
+					height="80"
+					patternUnits="userSpaceOnUse"
+				>
+					<rect width="80" height="80" fill="url(#smallGrid)" />
+					<path
+						d="M 80 0 L 0 0 0 80"
+						fill="none"
+						stroke="gray"
+						stroke-width="1"
+					/>
+				</pattern>
+			</defs>
+
+			<rect width="100%" height="100%" fill="url(#smallGrid)" />
+		</svg>`;
+
+		let DOMURL = window.URL || window.webkitURL || window;
+
+		let img = new Image();
+		let svg = new Blob([data], { type: "image/svg+xml;charset=utf-8" });
+		let url = DOMURL.createObjectURL(svg);
+
+		img.onload = () => {
+			this.canvasCtx.drawImage(img, 0, 0);
+			DOMURL.revokeObjectURL(url);
+		};
+		img.src = url;
 	}
 
 	update() {
@@ -250,11 +286,9 @@ class Block {
 		}
 
 		this.draw();
-	}
 
-	animate() {
-		this.animId = setInterval(() => {
+		requestAnimationFrame(() => {
 			this.update();
-		}, 1000 / 3);
+		});
 	}
 }
